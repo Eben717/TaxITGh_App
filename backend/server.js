5a7f77791553961dba1ac543deeefd5ac35b0a2e
Eben <kwabenat679@gmail.com>
2025-11-05 06:34:53 +0000

    created backend server file with dotenv config & port

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,13 @@
 import express from 'express';
 import { sql } from '../config/db.js';
 import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4001
+
+const app = express();
+
+app.listen(PORT, () => {
+    console.log('TaxITGh App Server is running on PORT 4000');
+});
