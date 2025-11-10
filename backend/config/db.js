e002e6b0bcff21e202290472ece41a3fd7f1970c
Eben <kwabenat679@gmail.com>
2025-11-10 23:38:18 +0000

    installed neon client and the server + database are live

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,8 @@
-import { neon } from "@neondatabase/serverless-client";
+import { neon } from "@neondatabase/serverless";
 
 import "dotenv/config";
 
 // CREATE sql connection using DB URL from .env file   
 export const sql = neon(process.env.DATABASE_URL);  
+
+export default sql;
