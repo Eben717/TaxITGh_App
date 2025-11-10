80b21b3b4209795e2a800d2f867551566742925c
Eben <kwabenat679@gmail.com>
2025-11-10 23:31:19 +0000

    database intialised in server

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,4 +3,4 @@ import { neon } from "@neondatabase/serverless-client";
 import "dotenv/config";
 
 // CREATE sql connection using DB URL from .env file   
-export const sql = neon(process.env.DATABASE_URL);
+export const sql = neon(process.env.DATABASE_URL);  
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import sql from './config/db.js';
 
 dotenv.config();
 
@@ -7,7 +8,28 @@ const PORT = process.env.PORT || 4001
 
 const app = express();
 
+async function initDB() {
+    try {
+        await sql`CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(100),
+            email VARCHAR(100) UNIQUE,
+            password VARCHAR(100)
+        )`;
+        console.log('Database initialized successfully');
+    }
+    catch (error) {
+        console.error('Error initializing database:', error);
+    }
+}
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the TaxITGh App Server');
+});
+
+initDB().then(() => {
 
 app.listen(PORT, () => {
     console.log('TaxITGh App Server is running on PORT 4001');
+});
 });
