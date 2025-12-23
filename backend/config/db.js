478784fc4843ce510c247feccbad986db2c72e9b
Eben <kwabenat679@gmail.com>
2025-12-23 18:21:42 +0000

    added sql data for companies in config.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -16,6 +16,20 @@ export async function initDB() {
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         )`;
         
+        await sql
+        `CREATE TABLE IF NOT EXISTS companies (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            address VARCHAR(255) NOT NULL,
+            registration_information VARCHAR(255) NOT NULL,
+            name_of_directors VARCHAR(255) NOT NULL,
+            name_of_auditors VARCHAR(255) NOT NULL,
+            industry_sector VARCHAR(255) NOT NULL,
+            contact_information VARCHAR(255) NOT NULL,
+            tax_identification_number VARCHAR(255) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )`;
+        
         console.log('Database initialized successfully');
     }
     catch (error) {
