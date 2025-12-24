ced58de1c22b04a625fa76ff7b9aaafafbe33201
Eben <kwabenat679@gmail.com>
2025-12-24 12:32:42 +0000

    added sql data for company tax, tax type and tax calculations

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,6 +7,7 @@ export const sql = neon(process.env.DATABASE_URL);
 
 export async function initDB() {
     try {
+        //users database table
         await sql
         `CREATE TABLE IF NOT EXISTS users (
             id SERIAL PRIMARY KEY,
@@ -16,6 +17,7 @@ export async function initDB() {
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         )`;
         
+        //companies database table
         await sql
         `CREATE TABLE IF NOT EXISTS companies (
             id SERIAL PRIMARY KEY,
@@ -27,9 +29,66 @@ export async function initDB() {
             industry VARCHAR(255) NOT NULL,
             contact_information VARCHAR(255) NOT NULL,
             tax_identification_number VARCHAR(255) NOT NULL,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            user_id INTEGER NOT NULL,
+            CONSTRAINT fk_company_user
+            FOREIGN KEY (user_id)
+            REFERENCES users(id)
+            ON DELETE CASCADE
         )`;
         
+        // Company tax data database table
+        await sql
+        `CREATE TABLE IF NOT EXISTS company_tax (
+        id SERIAL PRIMARY KEY,
+        company_id INTEGER NOT NULL,
+        tax_type_id INTEGER NOT NULL,
+        applicable_from DATE NOT NULL,
+        applicable_to DATE,
+        status VARCHAR(30) NOT NULL DEFAULT 'active',
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+        CONSTRAINT fk_companytax_company
+          FOREIGN KEY (company_id)
+          REFERENCES companies(id)
+          ON DELETE CASCADE,
+
+        CONSTRAINT fk_companytax_taxtype
+          FOREIGN KEY (tax_type_id)
+          REFERENCES tax_types(id)
+          ON DELETE RESTRICT
+      );
+    `;
+
+        //Tax types database table
+        await sql
+        `CREATE TABLE IF NOT EXISTS tax_types (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(100) NOT NULL,
+            description TEXT,
+            rate DECIMAL(5,2) NOT NULL,
+            category VARCHAR(50),
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )`;
+
+        //Tax calculations database table
+        await sql
+        `CREATE TABLE IF NOT EXISTS tax_calculations (
+        id SERIAL PRIMARY KEY,
+        company_tax_id INTEGER NOT NULL,
+        taxable_income DECIMAL(15,2) NOT NULL,
+        tax_rate DECIMAL(5,2) NOT NULL,
+        amount DECIMAL(15,2) NOT NULL,
+        tax_period VARCHAR(50) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        CONSTRAINT fk_taxcalc_companytax
+        FOREIGN KEY (company_tax_id)
+        REFERENCES company_tax(id)
+        ON DELETE CASCADE
+      );
+    `;
+
+
         console.log('Database initialized successfully');
     }
     catch (error) {
